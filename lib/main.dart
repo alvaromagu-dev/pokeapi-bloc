e7df5617cc612840b2f2438a9bd39dae0a371882
alvaro <alvaromagudev@gmail.com>
2022-07-06 11:11:31 +0200

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:check_install/routes/routes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const CounterApp());
+  runApp(const PokemonApp());
 }
 
-class CounterApp extends StatelessWidget {
-  const CounterApp({Key? key}) : super(key: key);
+class PokemonApp extends StatelessWidget {
+  const PokemonApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
