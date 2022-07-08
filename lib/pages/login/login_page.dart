dd10a78f0812fc428324632b6f530eeb8cd79b83
alvaro <alvaromagudev@gmail.com>
2022-07-08 20:53:24 +0200

    login form

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,3 +1,6 @@
+import 'package:check_install/routes/paths.dart';
+import 'package:check_install/widgets/list_with_footer.dart';
+import 'package:check_install/widgets/password_input.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,13 +11,86 @@ class LoginPage extends StatefulWidget {
 }
 
 class LoginPageState extends State<LoginPage> {
+  bool keepLogged = false;
+
+  void onKeepLoggedChange(value) => setState(() => keepLogged = value);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: const Text('Shopping'),
       ),
-      body: Column(),
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(
+            maxWidth: 600,
+            maxHeight: 800
+          ),
+          child: ListWithFooter(
+            footer: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(pokemons);
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text('LOGIN'),
+                  )
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed(register);
+                },
+                child: const Text('Don\'t have account? Register now!'),
+              ),
+            ],
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  'Login',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
+                    labelText: 'Username'
+                  ),
+                ),
+              ),
+              const PasswordInput(
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.password),
+                padding: EdgeInsets.all(10),
+                labelText: 'Password',
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: CheckboxListTile(
+                  value: keepLogged,
+                  onChanged: onKeepLoggedChange,
+                  title: const Text('Keep logged?'),
+                  shape: const RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.grey,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(5))
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/pages/register/register_page.dart
+++ lib/pages/register/register_page.dart
@@ -66,7 +66,7 @@ class _RegisterPageState extends State<RegisterPage> {
                         return;
                       }
 
-                      Navigator.of(context).pushNamed(pokemons);
+                      Navigator.of(context).pop();
                     },
                     child: const Padding(
                       padding: EdgeInsets.symmetric(vertical: 15),
